70f6e2365c39c3238811a11d5d063eac182f3539
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-10-19 19:26:18 +0100

    Health Bar

--- Assets/Scripts/Enemy/TempScript.cs
+++ Assets/Scripts/Enemy/TempScript.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TempScript : MonoBehaviour
+namespace Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TempScript : MonoBehaviour
     {
         
-    }
+        void Start()
+        {
+        
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         
+        void Update()
+        {
+            
+            
+        }
     }
 }
---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Player
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private const float MaxHealth = 200f;
+        [Range(0,MaxHealth)]
+        private static float _health = MaxHealth;
+        private static Image _healthBarImg;
+        
+        void Start()
+        {
+            _healthBarImg = GetComponent<Image>();
+        }
+
+        
+        void Update()
+        {
+            
+        }
+        
+        public static float Health
+        {
+            get => _health;
+            set
+            {
+                // Clamp health between 0 and MaxHealth
+                _health = Math.Clamp(value, 0, MaxHealth);
+                
+                // Update health UI
+                var healthPercentage = _health / MaxHealth;
+                _healthBarImg.fillAmount = healthPercentage;
+                
+                _healthBarImg.color = new Color(23f / 255f, 108f / 255f, 12f / 255f);
+                
+                if (healthPercentage < 0.5f)
+                {
+                    _healthBarImg.color = Color.yellow;
+                }
+                if (healthPercentage < 0.25f)
+                {
+                    _healthBarImg.color = Color.red;
+                }
+                
+                // If health is 0, kill the player
+                if (_health <= 0)
+                {
+                    //TODO: Kill the player
+                    Debug.Log("Player is dead!");
+                }
+                
+            }
+            
+        }
+    }
+}
